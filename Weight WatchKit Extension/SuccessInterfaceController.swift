2807337efaa3f928c8c3ed85baf9b130f4825da0
Jackson-S <Jackson-S@users.noreply.github.com>
2019-06-14 14:55:14 +1000

    Reformat code for readability

--- Weight WatchKit Extension/SuccessInterfaceController.swift
+++ Weight WatchKit Extension/SuccessInterfaceController.swift
@@ -16,49 +16,54 @@ class SuccessInterfaceController: WKInterfaceController {
     @IBOutlet weak var bmiCategoryLabel: WKInterfaceLabel!
     @IBOutlet weak var motivationLabel: WKInterfaceLabel!
     
+    let defaultString = "---"
+    var resultParameters: ResultsParameters?
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        let defaultString = "---"
         
         if let parameters = context as? ResultsParameters {
-            if let weightKG = parameters.weightKG {
-                let weightLabelText = String.localizedStringWithFormat(LocalizedStrings.weightLabel, weightKG)
-                weightLabel.setText(weightLabelText)
-            } else {
-                weightLabel.setText(defaultString)
-            }
-            
-            if let bmi = parameters.bmi {
-                let bmiLabelText = String.localizedStringWithFormat("%.1f", bmi)
-                bmiLabel.setText(bmiLabelText)
-            } else {
-                bmiLabel.setText(defaultString)
-            }
-            
-            if let bmiCategory = parameters.bmiCategroy {
-                switch bmiCategory {
-                    case .Underweight:
-                        bmiCategoryLabel.setText(LocalizedStrings.underweightBmi)
-                    case .Normal:
-                        bmiCategoryLabel.setText(LocalizedStrings.normalBmi)
-                    case .Overweight:
-                        bmiCategoryLabel.setText(LocalizedStrings.overweightBmi)
-                    case .Obese:
-                        bmiCategoryLabel.setText(LocalizedStrings.obeseBmi)
-                }
-            } else {
-                bmiCategoryLabel.setText(defaultString)
-            }
-            
-            if let weight = parameters.weight, let oldWeight = parameters.oldWeight {
-                let difference = ((weight - oldWeight) / 100).rounded() / 10
-                let differenceLabelText = String.localizedStringWithFormat(LocalizedStrings.weightDifference, difference)
-                differenceLabel.setText(differenceLabelText)
-            } else {
-                differenceLabel.setText(defaultString)
-            }
+            self.resultParameters = parameters
+            updateDisplay(parameters)
         } else {
             print("Error recieving parameters")
+        }
+    }
+    
+    func updateDisplay(_ resultParameters: ResultsParameters) {
+        // Set all parameters to default in advance.
+        weightLabel.setText(defaultString)
+        bmiLabel.setText(defaultString)
+        bmiCategoryLabel.setText(defaultString)
+        differenceLabel.setText(defaultString)
+
+        if let weightKG = resultParameters.weightKG {
+            let weightLabelText = String.localizedStringWithFormat(LocalizedStrings.weightLabel, weightKG)
+            weightLabel.setText(weightLabelText)
+        }
+
+        if let bmi = resultParameters.bmi {
+            let bmiLabelText = String(format: "%.1f", bmi)
+            bmiLabel.setText(bmiLabelText)
+        }
+
+        if let bmiCategory = resultParameters.bmiCategroy {
+            switch bmiCategory {
+                case .Underweight:
+                    bmiCategoryLabel.setText(LocalizedStrings.underweightBmi)
+                case .Normal:
+                    bmiCategoryLabel.setText(LocalizedStrings.normalBmi)
+                case .Overweight:
+                    bmiCategoryLabel.setText(LocalizedStrings.overweightBmi)
+                case .Obese:
+                    bmiCategoryLabel.setText(LocalizedStrings.obeseBmi)
+            }
+        }
+
+        if let weight = resultParameters.weight, let oldWeight = resultParameters.oldWeight {
+            let difference = ((weight - oldWeight) / 100).rounded() / 10
+            let differenceLabelText = String.localizedStringWithFormat(LocalizedStrings.weightDifference, difference)
+            differenceLabel.setText(differenceLabelText)
         }
     }
 }
