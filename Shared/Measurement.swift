746ee96da29d68c0c614f45d2b682f8e4a28f391
Jackson-S <Jackson-S@users.noreply.github.com>
2019-06-24 11:56:57 +1000

    Add clamp method to built-in measurement type

---
+++ Shared/Measurement.swift
@@ -0,0 +1,28 @@
+//
+//  Measurement.swift
+//  Weight
+//
+//  Created by Jackson Sommerich on 24/6/19.
+//  Copyright © 2019 Jackson Sommerich. All rights reserved.
+//
+
+import Foundation
+import simd
+
+extension Measurement {
+    public func clamped(_ min: Measurement, _ max: Measurement) -> Measurement? {
+        guard min.unit == max.unit && min.value <= max.value else {
+            return nil
+        }
+        
+        return Measurement(value: simd_clamp(self.value, min.value, max.value), unit: self.unit)
+    }
+    
+    public func clamped(_ min: Double, _ max: Double) -> Measurement? {
+        guard min <= max else {
+            return nil
+        }
+        
+        return Measurement(value: simd_clamp(self.value, min, max), unit: self.unit)
+    }
+}
